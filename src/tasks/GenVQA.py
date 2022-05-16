3d1bcc815f37b14d7e7c3a1345e32622629f742e
Hadi Sheikhi <hadsheikhi@gmail.com>
2022-05-16 21:22:21 +0430

    implement validation

--- src/tasks/GenVQA.py
+++ src/tasks/GenVQA.py
@@ -17,12 +17,17 @@ class VQA:
         self.epochs = epochs
         self.batch_size = batch_size
         self.log_every = log_every
+        self.val_loader = None
+        if(val_dset):
+            self.val_loader = DataLoader(val_dset, batch_size=batch_size,
+            shuffle=False, drop_last=True, collate_fn=pad_batched_sequence)
         if(use_cuda):
             self.model = self.model.cuda()
         self.criterion = nn.CrossEntropyLoss()
         self.train_date_time = train_date
         self.optim = torch.optim.Adam(list(self.model.parameters()), lr=lr)
         self.save_dir = os.path.join(CHECKPOINTS_DIR, str(self.train_date_time))
+
     def train(self):
         runnnin_loss = 0.0
         running_accuracy = 0.0
@@ -31,30 +36,44 @@ class VQA:
                 self.model.train()
                 self.optim.zero_grad()
                 pbar.set_description(f"Epoch {epoch}")
-                target_masks = target_masks.unsqueeze(dim=2)
-                logits = self.model(input_ids, feats, boxes, masks, target)
-                target_labels = torch.nn.functional.one_hot(target, num_classes=self.model.Tokenizer.vocab_size).double()
-                logits = logits * target_masks
-                target_labels = target_labels*target_masks
-
-                loss = self.criterion(logits, target_labels)
-                loss.backward()
-                self.optim.step()
+                loss, batch_acc = self.__step(input_ids, feats, boxes, masks, target, target_masks, val=False)  
                 runnnin_loss += loss.item()
-
-                #calculate accuracy
-                pred = torch.argmax(logits, dim=-1)
-                true_predictions = torch.sum((pred == target) * target_masks.squeeze())
-                running_accuracy += true_predictions / (self.batch_size * torch.sum(target_masks))
-                batch_acc = true_predictions / (self.batch_size * torch.sum(target_masks))
-                assert batch_acc <= 1  
+                running_accuracy += batch_acc
             if epoch % self.log_every == self.log_every - 1:
+                val_loss = None
+                val_acc = None
+                if(self.val_loader):
+                    self.model.eval()
+                    val_loss = 0
+                    val_acc = 0
+                    for i, (input_ids, feats, boxes, masks, target, target_masks) in enumerate(self.val_loader):
+                        val_loss, val_acc_batch = self.__step(input_ids, feats, boxes, masks, target, target_masks, val=True)
+                        val_loss += loss.item()
+                        val_acc += val_acc_batch
+                    val_acc /= len(self.val_loader)
+
                 total_data_iterated = self.log_every * len(self.train_loader)
-                Logger.log("Train", f"Training epoch {epoch} with loss {runnnin_loss / self.log_every:.3f} with accuracy {running_accuracy / total_data_iterated:.3f}")
+                Logger.log("Train", f"Training epoch {epoch}: train loss {runnnin_loss / self.log_every:.3f}. Val loss: {val_loss:.3f}."
+                + f" Train accuracy {running_accuracy / total_data_iterated:.3f}. Val accuracy: {val_acc}")
                 self.model.save(self.save_dir, epoch)
                 runnnin_loss = 0.0
                 running_accuracy = 0.0
-        
+    
+    def __step(self, input_ids, feats, boxes, masks, target, target_masks, val=False):
+        logits = self.model(input_ids, feats, boxes, masks, target)
+        target_masks = target_masks.unsqueeze(2)
+        target_labels = torch.nn.functional.one_hot(target, num_classes=self.model.Tokenizer.vocab_size).double()
+        logits = logits * target_masks
+        target_labels = target_labels*target_masks
+        loss = self.criterion(logits, target_labels)
+        if not(val):
+            loss.backward()
+            self.optim.step()
+        pred = torch.argmax(logits, dim=-1)
+        true_predictions = torch.sum((pred == target) * target_masks.squeeze())
+        batch_acc = true_predictions / (self.batch_size * torch.sum(target_masks))
+        assert batch_acc <= 1
+        return loss, batch_acc
                 
 
 
@@ -63,9 +82,13 @@ class VQA:
 
 if __name__ == "__main__":
     model = LXMERT_LSTM.LXMERT_LSTM()
-    dset = GenVQADataset(model.Tokenizer, 
+    train_dset = GenVQADataset(model.Tokenizer, 
         annotations = "../fsvqa_data_train/annotations.pickle", 
         questions = "../fsvqa_data_train/questions.pickle", 
         img_dir = "../img_data")
-    vqa = VQA(datetime.now(), dset, model)
+    val_dset = GenVQADataset(model.Tokenizer, 
+        annotations = "../fsvqa_data_val/annotations.pickle", 
+        questions = "../fsvqa_data_val/questions.pickle", 
+        img_dir = "../img_data")
+    vqa = VQA(datetime.now(), train_dset, model, val_dset=val_dset)
     vqa.train()
