92033fb52bd59e74ecd4f3c5b8d7be002db7ea9e
Hadi Sheikhi <hadsheikhi@gmail.com>
2022-05-16 18:11:25 +0430

    implement masked accuracy and loss

--- src/tasks/GenVQA.py
+++ src/tasks/GenVQA.py
@@ -7,13 +7,14 @@ import torch
 from src.logger import Instance as Logger
 from src.constants import CHECKPOINTS_DIR
 class VQA:
-    def __init__(self, train_dset,  model, val_dset=None, tokenizer=None, use_cuda=True, batch_size=32, epochs=200, lr=0.005):
+    def __init__(self, train_dset,  model, val_dset=None, tokenizer=None, use_cuda=True, batch_size=32, epochs=200, lr=0.005, log_every=1):
         
         self.train_loader = DataLoader(train_dset, batch_size=batch_size,
             shuffle=True, drop_last=True, collate_fn=pad_batched_sequence)
         self.model = model
         self.epochs = epochs
         self.batch_size = batch_size
+        self.log_every = log_every
         if(use_cuda):
             self.model = self.model.cuda()
         self.criterion = nn.CrossEntropyLoss()
@@ -23,11 +24,16 @@ class VQA:
         runnnin_loss = 0.0
         running_accuracy = 0.0
         for epoch in range(self.epochs):
-            for i, (input_ids, feats, boxes, masks, target, target_masks) in enumerate(tqdm(self.train_loader, total=len(self.train_loader))):
+            for i, (input_ids, feats, boxes, masks, target, target_masks) in enumerate(pbar := tqdm(self.train_loader, total=len(self.train_loader))):
                 self.model.train()
                 self.optim.zero_grad()
+                pbar.set_description(f"Epoch {epoch}")
+                target_masks = target_masks.unsqueeze(dim=2)
                 logits = self.model(input_ids, feats, boxes, masks, target)
                 target_labels = torch.nn.functional.one_hot(target, num_classes=self.model.Tokenizer.vocab_size).double()
+                logits = logits * target_masks
+                target_labels = target_labels*target_masks
+
                 loss = self.criterion(logits, target_labels)
                 loss.backward()
                 self.optim.step()
@@ -35,10 +41,13 @@ class VQA:
 
                 #calculate accuracy
                 pred = torch.argmax(logits, dim=-1)
-                true_predictions = torch.sum((pred == target) * masks)
-                running_accuracy += true_predictions / self.batch_size
-            if epoch % 1 == 0:
-                Logger.log("Train", f"Training epoch {epoch} with loss {runnnin_loss / 1:.3f} with accuracy {running_accuracy / 1:.3f}")
+                true_predictions = torch.sum((pred == target) * target_masks.squeeze())
+                running_accuracy += true_predictions / (self.batch_size * torch.sum(target_masks))
+                batch_acc = true_predictions / (self.batch_size * torch.sum(target_masks))
+                assert batch_acc <= 1  
+            if epoch % self.log_every == self.log_every - 1:
+                total_data_iterated = self.log_every * len(self.train_loader)
+                Logger.log("Train", f"Training epoch {epoch} with loss {runnnin_loss / self.log_every:.3f} with accuracy {running_accuracy / total_data_iterated:.3f}")
                 self.model.save(CHECKPOINTS_DIR, epoch)
                 runnnin_loss = 0.0
                 running_accuracy = 0.0
