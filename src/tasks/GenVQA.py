ead2cb685a4937da46766c0f076c90f25bbfe75b
maryamhashemi <hashemi.maryam.sadat@gmail.com>
2022-06-08 08:22:29 +0430

    use Accuracy metric from torchmetrics instead of calculate accuracy from scratch.

--- src/tasks/GenVQA.py
+++ src/tasks/GenVQA.py
@@ -1,55 +1,62 @@
-import torch.nn as nn
-from torch.utils.data.dataloader import DataLoader
-from tqdm import tqdm
-from src.models import LXMERT_LSTM
-from src.data.datasets import GenVQADataset, pad_batched_sequence
-import torch
-from src.logger import Instance as Logger
-from src.constants import CHECKPOINTS_DIR
-from datetime import datetime
 import os
-from torchmetrics import F1Score
+from datetime import datetime
+
+import torch
+import torch.nn as nn
+from src.constants import CHECKPOINTS_DIR
+from src.data.datasets import GenVQADataset, pad_batched_sequence
+from src.logger import Instance as Logger
+from src.models import LXMERT_LSTM
+from torch.utils.data.dataloader import DataLoader
+from torchmetrics import F1Score, Accuracy
+from tqdm import tqdm
+
+
 class VQA:
-    def __init__(self, train_date, train_dset,  model, 
-                val_dset=None, 
-                tokenizer=None, 
-                use_cuda=True, 
-                batch_size=32, 
-                epochs=200, 
-                lr=0.005, 
-                log_every=1, 
-                save_every=50):
+    def __init__(self,
+                 train_date,
+                 model,
+                 train_dset,
+                 val_dset=None,
+                 tokenizer=None,
+                 use_cuda=True,
+                 batch_size=32,
+                 epochs=200,
+                 lr=0.005,
+                 log_every=1,
+                 save_every=50):
         
-        self.train_loader = DataLoader(train_dset, batch_size=batch_size,
-            shuffle=True, drop_last=True, collate_fn=pad_batched_sequence)
         self.model = model
         self.epochs = epochs
         self.batch_size = batch_size
         self.log_every = log_every
-        self.val_loader = None
+        self.train_date_time = train_date
+        self.save_every = save_every
         
-        if(val_dset):
-            self.val_loader = DataLoader(val_dset, batch_size=batch_size,
-            shuffle=False, drop_last=True, collate_fn=pad_batched_sequence)
+        self.train_loader = DataLoader(train_dset, batch_size=batch_size, shuffle=True, drop_last=True, collate_fn=pad_batched_sequence)
+        self.val_loader = DataLoader(val_dset, batch_size=batch_size, shuffle=False, drop_last=True, collate_fn=pad_batched_sequence)
+        
         if(use_cuda):
             self.model = self.model.cuda()
-        
-        self.criterion = nn.NLLLoss(ignore_index=0, size_average=True)
+            
+        pad_idx = 0
+        self.criterion = nn.CrossEntropyLoss(ignore_index=pad_idx)
         self.optim = torch.optim.Adam(list(self.model.parameters()), lr=lr)
-        self.train_date_time = train_date
+        
+        self.f1_score = F1Score(num_classes=self.model.Tokenizer.vocab_size, ignore_index=pad_idx, top_k=1, mdmc_average='samplewise')
+        self.accuracy = Accuracy(num_classes=self.model.Tokenizer.vocab_size, ignore_index=pad_idx, top_k=1, mdmc_average='samplewise')
+        
         self.save_dir = os.path.join(CHECKPOINTS_DIR, str(self.train_date_time))
-        self.save_every = save_every
-        self.f1_score = F1Score(num_classes=self.model.Tokenizer.vocab_size, ignore_index=0, top_k=1, mdmc_average='samplewise')
+        
     def train(self):
         runnnin_loss = running_accuracy = running_accuracy_best = running_f1 = 0
 
         for epoch in range(self.epochs):
-            
             for i, (input_ids, feats, boxes, masks, target, target_masks) in enumerate(pbar := tqdm(self.train_loader, total=len(self.train_loader))):
+
                 self.model.train()
-                self.optim.zero_grad()
-                pbar.set_description(f"Epoch {epoch}")
                 
+                pbar.set_description(f"Epoch {epoch}")
                 loss, batch_acc, batch_f1 = self.__step(input_ids, feats, boxes, masks, target, target_masks, val=False)  
                 
                 runnnin_loss += loss.item()
@@ -97,22 +104,21 @@ class VQA:
                 self.model.save(self.save_dir, epoch)
             
     
-    def __step(self, input_ids, feats, boxes, masks, target, target_masks, val=False):
-        target_lens = torch.sum(target_masks, dim=1)
-        
-        target_one_hot = torch.nn.functional.one_hot(target, num_classes=self.model.Tokenizer.vocab_size).double()
-        
-        logits = self.model(input_ids, feats, boxes, masks, target, target_lens)
-        loss = self.criterion(logits.permute(0, 2, 1), target)
-        
+    def __step(self, input_ids, feats, boxes, masks, target, target_masks, val=False):        
+        logits = self.model(input_ids, feats, boxes, masks, target)
+        # logits shape: (L, N, target_vocab_size)
+        loss = self.criterion(logits.permute(1, 2, 0), target.permute(1,0))
+
         if not(val):
+            self.optim.zero_grad()
             loss.backward()
             self.optim.step()
         
-        f1_score = self.f1_score(logits.permute(0, 2, 1), target)
-        pred = torch.argmax(logits, dim=-1)
-        true_predictions = torch.sum((pred == target) * target_masks)
-        batch_acc = true_predictions / (self.batch_size * torch.sum(target_masks))
+        f1_score = self.f1_score(logits.permute(1,2,0), target.permute(1,0))
+        batch_acc = self.accuracy(logits.permute(1,2,0), target.permute(1,0))
+        # pred = torch.argmax(logits, dim=-1)
+        # true_predictions = torch.sum((pred == target) * target_masks)   
+        # batch_acc = true_predictions / (self.batch_size * torch.sum(target_masks))
         
         assert batch_acc <= 1
         return loss, batch_acc, f1_score
@@ -132,5 +138,5 @@ if __name__ == "__main__":
         annotations = "../fsvqa_data_val/annotations.pickle", 
         questions = "../fsvqa_data_val/questions.pickle", 
         img_dir = "../val_img_data")
-    vqa = VQA(datetime.now(), train_dset, model, val_dset=val_dset)
+    vqa = VQA(datetime.now(),model, train_dset, val_dset=val_dset)
     vqa.train()
