2b58143bd10e7fcbed4f0c488c44d0fc65c6ba4a
maryamhashemi <hashemi.maryam.sadat@gmail.com>
2022-06-08 08:01:27 +0430

    add dropout, num-layer and bidirectional to lstm model. send a single word to lstm instead of a sentence.

--- src/models/LSTM.py
+++ src/models/LSTM.py
@@ -4,27 +4,39 @@ import torch.nn as nn
 from torch.nn import LSTM
 from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence
 class LSTMModel(torch.nn.Module):
-    def __init__(self, input_size = 768, hidden_size=768, output_size=768):
+    def __init__(self,embedding, input_size=768, hidden_size=768, output_size=768, num_layers=1, bidirectional=False, p=0.5):
         super().__init__()
+        self.embedding = embedding
         self.hidden_size = hidden_size
         self.output_size = output_size
         self.input_size = input_size
-        self.LSTM = LSTM(input_size=input_size, hidden_size=self.hidden_size, batch_first=True, bidirectional=False, num_layers=1)
+        self.num_layers = num_layers
+        self.bidirectional = bidirectional
+        
+        self.dropout = nn.Dropout(p)
+        self.LSTM = LSTM(input_size=input_size, hidden_size=self.hidden_size, 
+                         bidirectional=self.bidirectional, 
+                         num_layers=self.num_layers, dropout = p)
         self.Linear = nn.Linear(self.hidden_size, output_size)
-        self.log_softmax = nn.LogSoftmax(dim=-1)
+        # self.log_softmax = nn.LogSoftmax(dim=-1)
     
-    def forward(self, x, encoder_output, x_lens):
+    def forward(self, x, hidden, cell):
         """
-            Predicts the values based on the true labels. This function is proper for 
-            training phase. You have to use predict() for testing or interence phase.
+            # x shape: (N) where N is for batch size, we want it to be (1, N), seq_length
+            # is 1 here because we are sending in a single word and not a sentence
+        """
+        x = x.unsqueeze(0)
+        embedding = self.dropout(self.embedding(x))
+        # embedding shape: (1, N, embedding_size)
+        
+        outputs, (hidden, cell) = self.LSTM(embedding, (hidden, cell))
+        # outputs shape: (1, N, hidden_size)
+        
+        predictions = self.Linear(outputs)
+        
+        # predictions shape: (1, N, length_target_vocabulary) to send it to
+        # loss function we want it to be (N, length_target_vocabulary) so we're
+        # just gonna remove the first dim
+        predictions = predictions.squeeze(0)
 
-            x : Input Tensor of size (L, N, embedding_size)
-            encoder_output: encode output of size (N, hidden_size)
-        """
-        h0 = encoder_output.unsqueeze(0)
-        c0 = torch.zeros(*h0.shape).cuda()
-        x = pack_padded_sequence(x, x_lens.cpu(), batch_first=True, enforce_sorted=False)
-        output, _ = self.LSTM(x, (h0, c0))
-        output, _ = pad_packed_sequence(output, batch_first=True)
-        output = self.Linear(output)
-        return self.log_softmax(output)
+        return predictions, hidden, cell
