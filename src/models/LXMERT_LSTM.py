d4382b65ffc0f9b03827bc83441e2b2ee93ffd2e
maryamhashemi <hashemi.maryam.sadat@gmail.com>
2022-06-08 08:17:16 +0430

    add teacher forcing. feed lstm as decoder word by word.

--- src/models/LXMERT_LSTM.py
+++ src/models/LXMERT_LSTM.py
@@ -2,6 +2,8 @@ from transformers import LxmertTokenizer, LxmertModel
 import torch
 from src.models.LSTM import LSTMModel
 import os
+import random
+
 class LXMERT_LSTM(torch.nn.Module):
     def __init__(self, freeze_lxmert=True):
         super().__init__()
@@ -13,12 +15,12 @@ class LXMERT_LSTM(torch.nn.Module):
             for p in self.LXMERT.parameters():
                 p.requires_grad = False
         
-        self.embedding_layer = list(self.LXMERT.children())[0].word_embeddings
-        self.LSTM = LSTMModel(output_size=self.Tokenizer.vocab_size)
+        embedding_layer = list(self.LXMERT.children())[0].word_embeddings
+        self.LSTM = LSTMModel(embedding=embedding_layer, output_size=self.Tokenizer.vocab_size)
         self.name = "LXMERT_LSTM"
 
     
-    def forward(self, input_ids, visual_feats, visual_pos, attention_mask, answer_tokenized, target_lens):
+    def forward(self, input_ids, visual_feats, visual_pos, attention_mask, answer_tokenized, teacher_force_ratio=0.5):
         """
             Train phase forward propagation
         """
@@ -28,13 +30,49 @@ class LXMERT_LSTM(torch.nn.Module):
             "visual_pos" : visual_pos,
             "attention_mask": attention_mask
         }
-        answer_embeddings = self.embedding_layer(answer_tokenized)
-        output = self.LXMERT(**kwargs)
-        output = output.pooled_output
         
-        output = self.LSTM(answer_embeddings, output, target_lens)
+        batch_size = answer_tokenized.shape[1]
+        target_len = answer_tokenized.shape[0]
+        target_vocab_size = self.Tokenizer.vocab_size
+        
+        outputs = torch.zeros(target_len, batch_size, target_vocab_size).cuda()
+        
+        #encoder
+        output = self.LXMERT(**kwargs)
+        encoder_output = output.pooled_output
+        # encoder_output shape: (N, hidden_size) to send it to
+        # Decoder as hidden we want it to be (D*num_layers, N, hidden_size) so we're
+        # just gonna expand it.
+        # D = 2 if bidirectional=True otherwise 1
+        
+        D = 2 if self.LSTM.bidirectional==True else 1
+        
+        hidden = encoder_output.expand(D*self.LSTM.num_layers, -1, -1)
+        # hidden shape: (D*num_layers, N, hidden_size)
+        cell = torch.zeros(*hidden.shape).cuda()
+        
+        # Grab the first input to the Decoder which will be <SOS> token
+        x = answer_tokenized[0]
+        
+        for t in range(1, target_len):
+            # Use previous hidden, cell as context from encoder at start
+            output, hidden, cell = self.LSTM(x, hidden, cell)
 
-        return output
+            # Store next output prediction
+            outputs[t] = output
+
+            # Get the best word the Decoder predicted (index in the vocabulary)
+            best_guess = output.argmax(1)
+
+            # With probability of teacher_force_ratio we take the actual next word
+            # otherwise we take the word that the Decoder predicted it to be.
+            # Teacher Forcing is used so that the model gets used to seeing
+            # similar inputs at training and testing time, if teacher forcing is 1
+            # then inputs at test time might be completely different than what the
+            # network is used to. This was a long comment.
+            x = answer_tokenized[t] if random.random() < teacher_force_ratio else best_guess
+
+        return outputs
 
 
     def save(self, dir_, epoch):
