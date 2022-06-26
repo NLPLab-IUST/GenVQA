331e8aa7954fffeb3dd68c1792f100d69bf8ba94
nikinezakati <nikinezakati@Nikis-MacBook-Pro-2.local>
2022-06-26 16:19:25 +0430

    Implemented metric calculator

--- src/metrics/MetricCalculator.py
+++ src/metrics/MetricCalculator.py
@@ -8,7 +8,7 @@ from src.metrics.EmbeddingBase.GreedyMatchingScore import GreedyMatchingScore
 #!pip install rouge_score
 #!pip install bert_score
 
-class MetricCalculator:
+class MetricCalculator():
     def __init__(self, tokenizer, embedding_layer) -> None:
         self.tokenizer = tokenizer
         self.embedding_layer = embedding_layer
@@ -53,14 +53,14 @@ class MetricCalculator:
 
 
 
-    if __name__ == '__main__':
-        from transformers import LxmertTokenizer, LxmertModel
+if __name__ == '__main__':
+    from transformers import LxmertTokenizer, LxmertModel
 
-        tokenizer = LxmertTokenizer.from_pretrained("unc-nlp/lxmert-base-uncased")
-        model = LxmertModel.from_pretrained("unc-nlp/lxmert-base-uncased")
-        E = model.embeddings.word_embeddings
+    tokenizer = LxmertTokenizer.from_pretrained("unc-nlp/lxmert-base-uncased")
+    model = LxmertModel.from_pretrained("unc-nlp/lxmert-base-uncased")
+    E = model.embeddings.word_embeddings
 
-        mc = MetricCalculator(tokenizer, E)
-        preds = ['the cat is on the mat', 'the cat is on the mat']
-        target = ['there is a catty on the matew', 'a cat is on the mat']
-        mc.compute(preds, target)
+    mc = MetricCalculator(tokenizer, E)
+    preds = ['the cat is on the mat', 'the cat is on the mat']
+    target = ['there is a catty on the matew', 'a cat is on the mat']
+    mc.compute(preds, target)
